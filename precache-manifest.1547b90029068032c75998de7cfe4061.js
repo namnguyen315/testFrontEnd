73d563e43b849a8a0699561017cc19aed7058392
namnguyen315 <namnguyen3152001xtnd@gmail.com>
2022-10-08 18:08:24 +0700

    Updates

---
+++ precache-manifest.1547b90029068032c75998de7cfe4061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6381e768b667604f1ac",
+    "url": "/static/css/main.8291fc30.chunk.css"
+  },
+  {
+    "revision": "e6381e768b667604f1ac",
+    "url": "/static/js/main.e6381e76.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf6a5690b9d5b5461eae08f317c88d1f",
+    "url": "/index.html"
+  }
+];
